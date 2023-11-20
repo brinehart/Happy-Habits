c3d3353812c55d00469b1dbb24e95eb2564648ee
brinehart <ben@brinehartdesign.com>
2023-11-20 08:25:27 -0500

    Add KidAvatarSelectComponent

---
+++ frontend/libs/kids/src/lib/components/manage/kid-avatar-select/kid-avatar-select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hh-kids-kid-avatar-select',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './kid-avatar-select.component.html',
+  styleUrls: ['./kid-avatar-select.component.scss']
+})
+export class KidAvatarSelectComponent {}
